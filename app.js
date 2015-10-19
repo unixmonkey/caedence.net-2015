9d752d26aaa01a941d16185fbd2211a953707d9e
David Jones <unixmonkey1@gmail.com>
2015-10-19 00:42:40 -0400

    Create Mongoose model for Note, and list notes.

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 var app = require('express')();
+var db = require('mongoose');
+db.connect('mongodb://localhost/caedence_net_development');
 
+var Note = db.model('Note', {
+  title: String,
+  body_html: String,
+  body_text: String,
+  user_id: Number,
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now }
+});
+
+// create some records with mongo cli like this:
+// $ mongod
+// > use caedence_net_development
+// > db.notes.insert({ title: "Hello", body_html: "<p>World</p>", body_text: "World" })
+// WriteResult({ "nInserted" : 1 })
+// > db.notes.find()
+// { "_id" : ObjectId("562473c0802fa33ca991217b"), "title" : "Hello", "body_html" : "<p>World</p>", "body_text" : "World" }
+app.get('/notes/', function(req, res) {
+  Note.find(function(err, notes) {
+    res.json(notes);
+  });
+});
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
