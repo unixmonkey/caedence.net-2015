b2ccd46e5cc0f5d4bb76301ead7f15cff9e6d00e
David Jones <unixmonkey1@gmail.com>
2015-10-24 18:37:22 -0400

    Remove intermediate variables since `require` will return the necessary objects.

--- server.js
+++ server.js
@@ -9,22 +9,15 @@ app.use(express.static(path.join(__dirname, 'client')));
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-var cors = require('./server/middlewares/cross_origin_requests');
-app.use(cors);
+// Middlewares
+app.use(require('./server/middlewares/cross_origin_requests'));
+app.use(require('./server/middlewares/http_verbs'));
+app.use(require('./server/middlewares/add_user_to_request'));
 
-var http_verbs = require('./server/middlewares/http_verbs');
-app.use(http_verbs);
-
-var add_user_to_request = require('./server/middlewares/add_user_to_request');
-app.use(add_user_to_request);
-
-// mount controller files
-var notes = require('./server/controllers/notes');
-app.use('/', notes);
-var users = require('./server/controllers/users');
-app.use('/', users);
-var session = require('./server/controllers/session');
-app.use('/', session);
+// Mount controller files
+app.use('/', require('./server/controllers/notes'));
+app.use('/', require('./server/controllers/users'));
+app.use('/', require('./server/controllers/session'));
 
 // Start server
 var server = app.listen(3000, function() {
