e946c2cf3c72d17998e13fa95bcb2b7b606e6c8c
David Jones <unixmonkey1@gmail.com>
2015-10-25 19:22:38 -0400

    Send 200 for cross-domain OPTIONS preflight requests.

--- server/middlewares/cross_origin_requests.js
+++ server/middlewares/cross_origin_requests.js
@@ -1,7 +1,12 @@
 // Allow serving from another domain or protocol
 module.exports = function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-  next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Request-Method', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 };
