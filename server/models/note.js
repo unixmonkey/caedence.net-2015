bd28d892075f3b2e1d2d1892c2d6c7666ff0d415
David Jones <unixmonkey1@gmail.com>
2015-10-25 13:37:49 -0400

    Separate models from their schemas
    So we can require and link their relationships to 
    [populate](http://mongoosejs.com/docs/populate.html)
    user data on notes.

--- server/models/note.js
+++ server/models/note.js
@@ -1,36 +1,5 @@
 var db = require('../db');
-var sanitizeHtml = require('sanitize-html');
-
-var NoteSchema = db.Schema({
-  title: String,
-  body_html: String,
-  body_text: String,
-  user_id: Number,
-  url: String,
-  created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now }
-});
-
-NoteSchema.pre('save', function(next) {
-  this.url = '/notes/' + this._id;
-  this.body_text = sanitizeHtml(this.body_html, {
-    allowedTags: [],
-    allowedAttributes: []
-  });
-  this.updated_at = Date.now;
-  next();
-});
-
-// Override toJSON to rename _id to id and __v to version
-NoteSchema.methods.toJSON = function() {
-  var object = this.toObject();
-  object.id = object._id;
-  delete object._id;
-  object.version = object.__v;
-  delete object.__v;
-  return object;
-}
-
+var NoteSchema = require('./note_schema')
 var Note = db.model('Note', NoteSchema);
 
 module.exports = Note;
--- server/models/user.js
+++ server/models/user.js
@@ -1,41 +1,5 @@
-var bcrypt = require('bcryptjs');
 var db = require('../db');
-
-var UserSchema = db.Schema({
-  username: { type: String, required: true, unique: true, index: true },
-  name: { type: String, required: true },
-  password_digest: { type: String, required: true },
-  api_key: String,
-  created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now }
-});
-
-UserSchema.pre('save', function(next) {
-  if (!this.api_key) {
-    // create bcrypt hash of hash for the api_key
-    this.api_key = bcrypt.hashSync(this.password_digest, 10);
-  }
-  this.updated_at = Date.now;
-  next();
-});
-
-// Override toJSON to exclude fields from Express res.json() and rename _id to id
-UserSchema.methods.toJSON = function() {
-  var object = this.toObject();
-  delete object.password_digest;
-  delete object.version;
-  object.id = object._id;
-  delete object._id;
-  return object;
-}
-
-// Create method to compare passed-in password to password_digest
-UserSchema.methods.authenticate = function(password, callback) {
-  bcrypt.compare(password, this.password_digest, function(err, isMatch) {
-    callback(isMatch);
-  });
-}
-
+var UserSchema = require('./user');
 var User = db.model('User', UserSchema);
 
 module.exports = User;
